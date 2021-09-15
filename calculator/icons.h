140631c1f0dd0c3857cebdd2296958dc181321ee
Devin Smith <devin@devinsmith.net>
2021-09-15 14:51:41 -0700

    Add calculator

---
+++ calculator/icons.h
@@ -0,0 +1,20 @@
+/*********** Generated on 2021/09/15 14:48:54 by reswrap version 6.0.0 *********/
+
+/* Created by reswrap from file bigcalc.gif */
+extern const unsigned char bigcalc[];
+
+/* Created by reswrap from file colors.gif */
+extern const unsigned char colors[];
+
+/* Created by reswrap from file information.gif */
+extern const unsigned char information[];
+
+/* Created by reswrap from file question.gif */
+extern const unsigned char question[];
+
+/* Created by reswrap from file tinycalc.gif */
+extern const unsigned char tinycalc[];
+
+/* Created by reswrap from file constmem.bmp */
+extern const unsigned char constmem[];
+
