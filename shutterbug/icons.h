db197ba4b7ef34b8485754a038751b05f3bc2382
Devin Smith <devin@devinsmith.net>
2021-09-15 14:23:00 -0700

    Add shutterbug

---
+++ shutterbug/icons.h
@@ -0,0 +1,35 @@
+/*********** Generated on 2021/09/15 13:53:42 by reswrap version 6.0.0 *********/
+
+/* Created by reswrap from file line_0.gif */
+extern const unsigned char line_0[];
+
+/* Created by reswrap from file line_1.gif */
+extern const unsigned char line_1[];
+
+/* Created by reswrap from file line_2.gif */
+extern const unsigned char line_2[];
+
+/* Created by reswrap from file line_3.gif */
+extern const unsigned char line_3[];
+
+/* Created by reswrap from file line_4.gif */
+extern const unsigned char line_4[];
+
+/* Created by reswrap from file line_5.gif */
+extern const unsigned char line_5[];
+
+/* Created by reswrap from file line_6.gif */
+extern const unsigned char line_6[];
+
+/* Created by reswrap from file line_7.gif */
+extern const unsigned char line_7[];
+
+/* Created by reswrap from file line_8.gif */
+extern const unsigned char line_8[];
+
+/* Created by reswrap from file shutterbug.gif */
+extern const unsigned char shutterbug[];
+
+/* Created by reswrap from file tinyshutterbug.gif */
+extern const unsigned char tinyshutterbug[];
+
