24fa5023e96fb35cad51bd0f97babc2a7b8de854
Devin Smith <devin@devinsmith.net>
2024-09-05 12:11:58 -0700

    waitpid requires sys/wait.h

--- adie/Adie.cpp
+++ adie/Adie.cpp
@@ -18,6 +18,9 @@
 * You should have received a copy of the GNU General Public License             *
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.         *
 ********************************************************************************/
+#ifndef WIN32
+#include <sys/wait.h>
+#endif
 #include "xincs.h"
 #include "fx.h"
 #include "fxkeys.h"
